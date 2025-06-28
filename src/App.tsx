f2f681844b58c5202956190894b8e77b15219ef7
Sophie Hu <113879830+sophieeeeeehu@users.noreply.github.com>
2025-06-28 13:26:21 -0700

    setting up for sophie

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import DictationPage from './pages/DictationPage';
 import HomePage from './pages/HomePage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import VocabPage from './pages/VocabPage';
+import RecordPage from './pages/RecordPage';
+import './App.css'
 
 export default function App() {
   return (
@@ -10,8 +12,9 @@ export default function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/play" element={<DictationPage/>}></Route>
-        <Route path="/viewVocab" element={<VocabPage/>}></Route>
+        <Route path="/play" element={<DictationPage />}></Route>
+        <Route path="/viewVocab" element={<VocabPage />}></Route>
+        <Route path="/record" element={<RecordPage />}></Route>
       </Routes>
     </Router>
   );
---
+++ src/pages/RecordPage.tsx
@@ -0,0 +1,7 @@
+export default function RecordPage() {
+    return (
+        <div className="">
+            <h1 className="bg-amber-300">Hello</h1>
+        </div>
+    )
+}
